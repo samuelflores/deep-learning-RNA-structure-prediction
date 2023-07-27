4502a207193cd93465c007a6c1779da3449d1f49
xinhexing <xinhusti@gmail.com>
2023-07-27 18:23:52 +0200

    Remove comments

--- data_generation/generate_data.py
+++ data_generation/generate_data.py
@@ -13,30 +13,9 @@ from Bio.PDB import PDBList, FastMMCIFParser
 # TODO correct bottaro's original chain IDs (if they're wrong) 
 
 # DONE parse every non-duplicate .align.pdb ATOM file for the sequence and residue numbers
-
 # DONE check that a) the chain is long enough to contain the residue number b) the first residues and number align c) the whole sequence matches
-
-# allow for residue number skipping in decoy generation. generate decoys only from the chain from which the tetraloop originates
-
-# update chain ID in bottaro's dataset
-
-# 1 -  6        Record name   "ATOM  "
-# 7 - 11        Integer       serial       Atom  serial number.
-# 13 - 16        Atom          name         Atom name.
-# 17             Character     altLoc       Alternate location indicator.
-# 18 - 20        Residue name  resName      Residue name.
-# 22             Character     chainID      Chain identifier.
-# 23 - 26        Integer       resSeq       Residue sequence number.
-# 27             AChar         iCode        Code for insertion of residues.
-# 31 - 38        Real(8.3)     x            Orthogonal coordinates for X in Angstroms.
-# 39 - 46        Real(8.3)     y            Orthogonal coordinates for Y in Angstroms.
-# 47 - 54        Real(8.3)     z            Orthogonal coordinates for Z in Angstroms.
-# 55 - 60        Real(6.2)     occupancy    Occupancy.
-# 61 - 66        Real(6.2)     tempFactor   Temperature  factor.
-# 77 - 78        LString(2)    element      Element symbol, right-justified.
-# 79 - 80        LString(2)    charge       Charge  on the atom.
-
-# every residue starts with P
+# DONE allow for residue number skipping in decoy generation. generate decoys only from the chain from which the tetraloop originates
+# DONE (not really, i just don't touch the original data) update chain ID in bottaro's dataset
 
 # Extract PDB data from cluster folder filenames
 def get_tloop_data(clusters_folder):
