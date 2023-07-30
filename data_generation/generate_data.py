4b69631213f6a4f13c9856aaac95570eb1274871
xinhexing <xinhusti@gmail.com>
2023-07-30 17:03:33 +0200

    Add fragment generation and CSV output

--- data_generation/generate_data.py
+++ data_generation/generate_data.py
@@ -6,8 +6,15 @@ import numpy.typing as npt
 import pandas as pd
 
 
+# [chain ids]
+# [cluster]
+# [residue names]
+# [residue numbers]
+# [insertion codes]
+
+
 def parse_pdb(filepath: str):
-    res_names, res_seqs = [], []
+    res_names, res_nums = [], []
     with open(filepath, 'r') as file:
         for line in file:
             if line.split()[0] == 'MODEL':
@@ -15,15 +22,17 @@ def parse_pdb(filepath: str):
         for line in file:
             if line.split()[0] == 'TER':
                 break
-            rec_name, res_name, res_seq, element = line[:6].strip(), line[17:20].strip(), int(line[22:26].strip()), line[13:16].strip()
-            if rec_name == 'ATOM' and element == 'P':
+            rec_name, res_name, res_num, element = line[:6].strip(), line[17:20].strip(), line[22:26].strip(), line[13:16].strip()
+            if rec_name == 'ATOM' and element == 'P': # All bases start with P
                 res_names += [res_name]
-                res_seqs += [res_seq]
-    return res_names, res_seqs
+                res_nums += [res_num]
+    clust_nums = [int(filepath.split('/')[-2][1:])]*8
+    chain_ids, ins_codes = ['']*8, ['']*8
+    return chain_ids, clust_nums, res_names, res_nums, ins_codes
 
 
 def parse_cif(filepath: str):
-    pdb_strand_ids, pdb_mon_ids, pdb_seq_nums, pdb_ins_codes = [], [], [], []
+    chain_ids, res_names, res_nums, ins_codes = [], [], [], []
     with open(filepath, 'r') as file:
         for line in file:
             if line.strip() == '_pdbx_poly_seq_scheme.hetero':
@@ -32,46 +41,50 @@ def parse_cif(filepath: str):
             if line.strip() == '#':
                 break
             data = line.split()
-            pdb_strand_id, pdb_mon_id, pdb_seq_num, pdb_ins_code = data[9], data[7], data[5], data[10]
-            if len(pdb_mon_id) > 1 or pdb_mon_id == '?':
+            chain_id, res_name, res_num, ins_code = data[9], data[7], data[5], data[10]
+            if len(res_name) > 1 or res_name == '?':
                 continue
-            pdb_strand_ids += [pdb_strand_id]
-            pdb_mon_ids += [pdb_mon_id]
-            pdb_seq_nums += [pdb_seq_num]
-            pdb_ins_codes += [pdb_ins_code]
-    return pdb_strand_ids, pdb_mon_ids, pdb_seq_nums, pdb_ins_codes
+            chain_ids += [chain_id]
+            res_names += [res_name]
+            res_nums += [res_num]
+            ins_codes += [ins_code]
+    clust_nums = [0]*len(res_names)
+    return chain_ids, clust_nums, res_names, res_nums, ins_codes
 
 
-def get_tloop_sequences(clusters_folder: str) -> dict[str, list[tuple[int, npt.ArrayLike]]]:
-    sequences = {}
-    for folder in os.listdir(clusters_folder):
-        cluster = int(folder[1:])
-        for file in os.listdir(f'{clusters_folder}/{folder}'):
+def get_tloop_sequences(clust_dir: str) -> dict[str, list[npt.ArrayLike]]:
+    seqs = {}
+    for folder in os.listdir(clust_dir):
+        for file in os.listdir(f'{clust_dir}/{folder}'):
             pdb_id = file[:4].lower()
-            if pdb_id not in sequences.keys():
-                sequences[pdb_id] = []
-            filepath = f'{clusters_folder}/{folder}/{file}'
-            array = np.row_stack(parse_pdb(filepath))
-            if not any(np.array_equal(array, i[1]) for i in sequences[pdb_id]):
-                sequences[pdb_id] += [(cluster, array)]
-    return sequences
+            if pdb_id not in seqs.keys():
+                seqs[pdb_id] = []
+            filepath = f'{clust_dir}/{folder}/{file}'
+            arr = np.row_stack(parse_pdb(filepath))
+            if not any(np.array_equal(arr, i) for i in seqs[pdb_id]):
+                seqs[pdb_id] += [arr]
+    return seqs
 
 
-def get_full_sequences(pdb_ids: list[str], structures_folder: str) -> dict[str, npt.ArrayLike]:
-    sequences = {}
+def get_full_sequences(pdb_ids: list[str], struct_dir: str) -> dict[str, npt.ArrayLike]:
+    seqs = {}
     for pdb_id in pdb_ids:
-        array = np.row_stack(parse_cif(f'{structures_folder}/{pdb_id}.cif'))
-        sequences[pdb_id] = array
-    return(sequences)
+        arr = np.row_stack(parse_cif(f'{struct_dir}/{pdb_id}.cif'))
+        seqs[pdb_id] = arr
+    return(seqs)
 
 
-def align_tloops(tloop_seqs, full_seqs) -> dict[str, npt.ArrayLike]:
-    sequences = {}
-    for pdb_id, full_arr in full_seqs.items():
-        full_resnames, full_resnums = full_arr[1], full_arr[2]
-        clusters = np.zeros((len(full_resnames),), dtype=int)
-        for cluster, tloop_arr in tloop_seqs[pdb_id]:
-            tloop_resnames, tloop_resnums = tloop_arr[0], tloop_arr[1]
+def remove_redundancy(full_seqs: dict[str, npt.ArrayLike]) -> dict[str, npt.ArrayLike]:
+    # split array into subarrays by chain name
+    pass
+
+
+def align_tloops_to_full(tloop_seqs, full_seqs) -> dict[str, npt.ArrayLike]:
+    seqs = full_seqs.copy()
+    for pdb_id, full_arr in seqs.items():
+        full_resnames, full_resnums = full_arr[2], full_arr[3]
+        for tloop_arr in tloop_seqs[pdb_id]:
+            tloop_resnames, tloop_resnums = tloop_arr[2], tloop_arr[3]
             possible_idxs = np.where(full_resnums == tloop_resnums[0])[0]
             for idx in possible_idxs:
                 idx_resnames = full_resnames[idx:idx+8]
@@ -80,41 +93,74 @@ def align_tloops(tloop_seqs, full_seqs) -> dict[str, npt.ArrayLike]:
                     len(idx_resnames) == 8 and len(idx_resnums) == 8 and
                     np.all(idx_resnames == tloop_resnames) and np.all(idx_resnums == tloop_resnums)
                     ):
-                    clusters[idx] = cluster
-        sequences[pdb_id] = np.vstack([full_arr, clusters])
-    return sequences
+                    full_arr[1, idx] = tloop_arr[1, 0]
+    return seqs
 
 
-# frag_len: int = 8
-# frag_extension = int((8-frag_len)/2)
-# clusters[idx-frag_extension] = cluster
+# TODO is this correct? even if the tloop is off-center, should the whole fragment still be counted as a tloop?
+def get_fragments(all_seqs, frag_len: int = 8) -> dict[str, list[npt.ArrayLike]]:
+    frag_extension = int((frag_len-8)/2)
+    seqs = {}
+    for pdb_id, arr in all_seqs.items():
+        frags = []
+        for i in range(len(arr[0])-frag_len+1):
+            frag = arr[:,i:i+frag_len].copy() # Assignment by value, not reference
+            if len(np.unique(frag[0])) > 1 or len(frag[0]) < frag_len: # Remove chain-crossing fragments
+                continue
+            frag[1,:] = frag[1, frag_extension] # Replace all cluster numbers with cluster ID, looking at offset
+            frags += [frag]
+        seqs[pdb_id] = frags
+    return seqs
 
 
-# TODO make this table better
-def array_to_df(array):
-    df = pd.DataFrame(columns=['pdb_id','array'])
-    df['pdb_id'] = array.keys()
-    arr_list = []
-    for arr in array.values():
-        arr_list += [[','.join(i) for i in arr.tolist()]]
-    df['array'] = arr_list
-    df = df.explode('array')
+def arrdict_to_df(arrdict: dict) -> pd.DataFrame:
+    df = pd.DataFrame(columns=['pdb_id','index','category','values'])
+    categories = ['chain_ids', 'clust_nums', 'res_names', 'res_nums', 'ins_codes']
+    for pdb_id, value in arrdict.items():
+        if type(value) is list:
+            for idx, arr in enumerate(value):
+                arr_stings = [','.join(i) for i in arr.tolist()]
+                entry = pd.DataFrame({'pdb_id': pdb_id, 'index':int(idx), 'category':[categories], 'values':[arr_stings]})
+                entry = entry.explode(['category', 'values'])
+                df = pd.concat([df, entry], ignore_index=True)
+        else:
+            arr_stings = [','.join(i) for i in value.tolist()]
+            entry = pd.DataFrame({'pdb_id': pdb_id, 'category':[categories], 'values':[arr_stings]})        
+            entry = entry.explode(['category', 'values'])
+            df = pd.concat([df, entry], ignore_index=True)
+    df = df.dropna(axis=1)
     return df
 
 
 def main(args):
-    tloop_sequences = get_tloop_sequences(args.clusters_folder)
-    pdb_ids = tloop_sequences.keys()
-    full_sequences = get_full_sequences(pdb_ids, args.structures_folder)
-    all_sequences = align_tloops(tloop_sequences, full_sequences, args.fragment_length)
-    np.savez('all_sequences.npz', **all_sequences)
-    array_to_df(all_sequences).to_csv('all_sequences_test.csv', sep='\t', index=False)
+    tloop_seqs = get_tloop_sequences(args.clusters_dir)
+    np.savez('tloop_seqs.npz', **tloop_seqs)
+    arrdict_to_df(tloop_seqs).to_csv('tloop_seqs.csv', sep='\t', index=False)
+    print('Tetraloop sequences retrieved')
+
+    pdb_ids = tloop_seqs.keys()
+
+    full_seqs = get_full_sequences(pdb_ids, args.structures_dir)
+    # full_seqs = remove_redundancy(full_seqs)
+    np.savez('full_seqs.npz', **full_seqs)
+    arrdict_to_df(full_seqs).to_csv('full_seqs.csv', sep='\t', index=False)
+    print('Full sequences retrieved')
+
+    all_seqs = align_tloops_to_full(tloop_seqs, full_seqs)
+    np.savez('all_seqs.npz', **all_seqs)
+    arrdict_to_df(all_seqs).to_csv('all_seqs.csv', sep='\t', index=False)
+    print('All sequences retrieved')
+
+    all_frags = get_fragments(all_seqs, args.fragment_length)
+    np.savez('all_frags.npz', **all_frags)
+    arrdict_to_df(all_frags).to_csv('all_frags.csv', sep='\t', index=False)
+    print('All fragments retrieved')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('clusters_folder', type=str)
-    parser.add_argument('structures_folder', type=str)
-    parser.add_argument('--fragment_length', type=int, default=8)
+    parser.add_argument('-c', '--clusters_dir', type=str, default='../../../all_clusters')
+    parser.add_argument('-s', '--structures_dir', type=str, default='../../../all_structures')
+    parser.add_argument('-f', '--fragment_length', type=int, default=8)
     args = parser.parse_args()
     main(args)
