40fd7fc3edd5b44ca6abbc4d15ef054da4387f3c
xinhexing <xinhusti@gmail.com>
2024-08-01 19:39:55 +0200

    Add more fragment lengths

--- data_generation/generate_fragments.sh
+++ data_generation/generate_fragments.sh
@@ -6,3 +6,8 @@ python generate_fragments.py
 python generate_fragments.py -f 10
 python generate_fragments.py -f 12
 python generate_fragments.py -f 14
+python generate_fragments.py -f 16
+python generate_fragments.py -f 18
+python generate_fragments.py -f 20
+python generate_fragments.py -f 22
+python generate_fragments.py -f 24
